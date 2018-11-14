91e922e5ed887fcf91ec97373aad40c6d14b8239
pragdave <dave@pragdave.me>
2018-11-14 11:25:18 -0600

    change from commenting out to using tag :skip"

--- ex01.exs
+++ ex01.exs
@@ -16,9 +16,9 @@ defmodule Ex01 do
 
   Do NOT use the Task or Agent libraries for this test.
   """
-  
+
   def counter(value \\ 0) do
-    # ...tour code
+    # ...your code
   end
 
   def new_counter(initial_value \\ 0) do
@@ -35,39 +35,38 @@ ExUnit.start()
 defmodule Test do
   use ExUnit.Case
 
-  # Start by uncommenting this test and getting it to pass
+  # Start by enabling this test and getting it to pass
   # This test assumes you have a function `counter` that can be spawned
   # and which handles the `{:next, from}` message
+
+  @tag :skip   # delete this line to enable the test that follows
+
   test "basic message interface" do
-    # count = spawn Ex01, :counter, []
-    # send count, { :next, self() }
-    # receive do
-    #   { :next_is, value } ->
-    #     assert value == 0
-    # end
-    # 
-    # send count, { :next, self() }
-    # receive do
-    #   { :next_is, value } ->
-    #     assert value == 1
-    # end
+    count = spawn Ex01, :counter, []
+    send count, { :next, self() }
+    receive do
+      { :next_is, value } ->
+        assert value == 0
+    end
+
+    send count, { :next, self() }
+    receive do
+      { :next_is, value } ->
+        assert value == 1
+    end
     IO.puts "missing first test"
   end
 
-  # then uncomment this one
+  # then enable this one
   # Now we add two new functions to Ex01 that wrap the use of
   # that counter function, making the overall API cleaner
+
+  @tag :skip
+
   test "higher level API interface" do
-    # count = Ex01.new_counter(5)
-    # assert  Ex01.next_value(count) == 5
-    # assert  Ex01.next_value(count) == 6
-    IO.puts "missing second test"
+    count = Ex01.new_counter(5)
+    assert  Ex01.next_value(count) == 5
+    assert  Ex01.next_value(count) == 6
   end
 
 end
-
-
-
-
-
-
--- ex02.exs
+++ ex02.exs
@@ -32,31 +32,35 @@ defmodule Test do
   You'll do this three times, in three different ways.
 
   """
-  
+
 
   @doc """
-  First uncomment this test. Here you will be inserting code
+  First get this test working. Here you will be inserting code
   to create and access the agent inline, in the test itself.
 
   Replace the placeholders with your code.
   """
-  # test "counter using an agent" do
-  #   { :ok, counter } = # « your code goes here »
-  #   value   = Agent.get_and_update(counter, «your code here»)
-  #   assert value == 0
-  #   value   = Agent.get_and_update(counter, «your code here»)
-  #   assert value == 1
-  # end
+
+  test "counter using an agent" do
+    { :ok, counter } = «your code goes here»
+    value   = Agent.get_and_update(counter, «your code here»)
+    assert value == 0
+    value   = Agent.get_and_update(counter, «your code here»)
+    assert value == 1
+  end
 
   @doc """
-  Next, uncomment this test, and add code to the Ex02 module at the
+  Next, enable this test, and add code to the Ex02 module at the
   top of this file to make those tests run.
   """
-  # test "higher level API interface" do
-  #   count = Ex02.new_counter(5)
-  #   assert  Ex02.next_value(count) == 5
-  #   assert  Ex02.next_value(count) == 6
-  # end
+
+  @tag :skip
+
+  test "higher level API interface" do
+    count = Ex02.new_counter(5)
+    assert  Ex02.next_value(count) == 5
+    assert  Ex02.next_value(count) == 6
+  end
 
   @doc """
   Last (for this exercise), we'll create a global counter by adding
@@ -64,16 +68,12 @@ defmodule Test do
   count, but how can you arrange things so that you don't need to pass
   that agent into calls to `global_next_value`?
   """
-  # test "global counter" do
-  #   Ex02.new_global_counter
-  #   assert Ex02.global_next_value == 0
-  #   assert Ex02.global_next_value == 1
-  #   assert Ex02.global_next_value == 2
-  # end
+
+  @tag :skip
+  test "global counter" do
+    Ex02.new_global_counter
+    assert Ex02.global_next_value == 0
+    assert Ex02.global_next_value == 1
+    assert Ex02.global_next_value == 2
+  end
 end
-
-
-
-
-
-
